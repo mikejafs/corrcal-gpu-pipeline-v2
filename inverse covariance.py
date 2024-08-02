357dbfdaf3f1b1a3e0b2cd3d86a3255c1f115c99
Mike <michael.jafs@mail.mcgill.ca>
2024-08-02 00:02:28 -0400

    Added inverse covariance python file to the repo

---
+++ inverse covariance.py
@@ -0,0 +1,13 @@
+"""Add working routines from mat inverse v8 and create class around them"""
+
+
+
+#add the full working routines here and create a class, then,
+#create another file that imports the class and uses them to perform the validation tests and timing tests
+
+
+#For benchmarking, should probably create a class that uses the benchmark code or maybe just a function for now
+#that is in its own folder called tests or something. The file name can be something like benchmark.py... We can expand
+#upon this later...
+
+
