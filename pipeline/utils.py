39f5dbb42fa76e07a6642752d2f7d4764df6cd17
Mike <michael.jafs@mail.mcgill.ca>
2025-03-07 21:20:08 -0500

    committing

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -23,9 +23,26 @@ def sparse_cov_times_vec(N, Del, Sig, N_inv, Del_prime, Sig_prime, vec, isinv, x
     else:
         del_tmp = xp.transpose(Del, [0, 2, 1]) @ vec
         sig_tmp = xp.sum(xp.transpose(Sig, [0, 2, 1]) @ vec, axis=0)
-        out = N * vec + Del_prime @ del_tmp + Sig_prime @ sig_tmp
+        out = N * vec + Del @ del_tmp + Sig @ sig_tmp
     # out = out.reshape(vec.shape[0], vec.shape[1])
     return out
+
+# def sparse_cov_times_vec(N, Del, Sig, N_inv, Del_prime, Sig_prime, vec, isinv, xp):
+#     if vec.ndim == 2:
+#         vec = vec.reshape(vec.shape[0], vec.shape[1], 1)
+#         N = N.reshape(vec.shape[0], vec.shape[1], 1)
+#         N_inv = N_inv.reshape(vec.shape[0], vec.shape[1], 1)
+#     else:
+#         pass
+#     if isinv:
+#         del_tmp = xp.transpose(Del_prime, [0, 2, 1]) @ vec
+#         sig_tmp = xp.sum(xp.transpose(Sig_prime, [0, 2, 1]) @ vec, axis=0)
+#         out = N_inv * vec - Del_prime @ del_tmp - Sig_prime @ sig_tmp
+#     else:
+#         del_tmp = xp.transpose(Del, [0, 2, 1]) @ vec
+#         sig_tmp = xp.sum(xp.transpose(Sig, [0, 2, 1]) @ vec, axis=0)
+#         out = N * vec + Del @ del_tmp + Sig @ sig_tmp
+#     return out
 
 """
 -----------------------------------------------------------------------------------------------------------------------
